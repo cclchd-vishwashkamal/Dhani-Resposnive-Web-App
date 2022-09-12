beca0b02039e79d4f56fb9d19639cbe92c28cbbc
Vishwashk14 <Vishwash.kamal@chaincodeconsulting.com>
2022-09-12 17:32:29 +0530

    Dhani-web Apps

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ['./dist/*.html'],
+    theme: {
+      screens: {
+        'supsm':'50px',
+        // => @media (min-width: 0px) { ... }
+      },
+      extend: {
+
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
